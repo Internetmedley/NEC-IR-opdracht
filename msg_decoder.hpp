cfe05d02e25b89772f90534fbee016688299c6ca
v1oopc-ubuntu <you@example.com>
2019-10-11 04:56:38 -0700

    Added decoder.cpp Changed msg_decoder.hpp

--- msg_decoder.hpp
+++ msg_decoder.hpp
@@ -1,12 +1,15 @@
 #ifndef MSG_DECODER_HPP
 #def MSG_DECODER_HPP
 
+#include "rots.hpp"
+#include "hwlib.hpp"
 //this is a class that reads pauses from the pause channel
 
 namespace NEC{
 class msg_decoder : public pause_listener{
 private:
-    msg_listener listener;
+    pause_listener listener;
+	rtos::channel<uint8_t , 4> buffer;
 public:
     msg_decoder( msg_listener & l ):
         listener( l )
