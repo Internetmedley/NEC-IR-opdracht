b0f4499f5b197d040b39c1d8588b4e02d96d2581
Nova5621 <novajonker@icloud.com>
2019-11-06 02:36:46 -0800

    the good stuff

--- Display/main.cpp
+++ Display/main.cpp
@@ -1,34 +1,75 @@
 #include "hwlib.hpp"
-#include "pool.hpp"
+#include "rtos.hpp"
 
+class Display : public rtos::task<>{
+private:
+    hwlib::glcd_oled & oled;
+    hwlib::terminal_from & window;
+    
+    rtos::channel< int, 8 > state_decider;
+    
+    void main(){
+        for(;;){
+            int state = state_decider.read();
+            oled.clear();
+            int time;
+            switch( state ){
+                case 1:
+                    time = state_decider.read();
+                    window << "\f Gegeven tijd: \n"
+                             << time << " \n";
+                    oled.flush();
+                    break;
+                case 2:
+                    window << "\f case 2";
+                    oled.flush();
+                    break;
+                case 3:
+                    window << "\f simpel text bericht. \n";
+                    oled.flush();
+                    break;
+                case 4:
+                    window << "\f case 4";
+                    oled.flush();
+                    break;
+            }
+        }
+    }
+    
+public:
+    Display( auto & oled, auto & window): 
+    task( "Display" ),
+    oled( oled ),
+    window( window ),
+    state_decider( this, "state decider" )
+   {}
+   
+   void write_int_to_channel(int i){
+       state_decider.write( i );
+   }
+};
 
-int main( void ){	
+int main(){	
    hwlib::wait_ms( 500 );   // wait for the PC console to start
    
    namespace target = hwlib::target;
 
    auto scl = target::pin_oc( target::pins::scl );
    auto sda = target::pin_oc( target::pins::sda );   
-   auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda( scl, sda );
+   auto i2c_bus = hwlib::i2c_bus_bit_banged_scl_sda( scl, sda ); //maakt i2cbus aan
    
-   auto oled = hwlib::glcd_oled( i2c_bus, 0x3c );  
+   auto oled = hwlib::glcd_oled( i2c_bus, 0x3c );  //maakt oled object aan
       
-   auto f1 = hwlib::font_default_16x16();
-   auto f2 = hwlib::font_default_8x8();
+   auto font = hwlib::font_default_8x8();  //letter grootte
    
-   auto d = hwlib::terminal_from( oled, f2 );   
-      
-//   int n = 0;   
-//   for(;;){
-//      hwlib::wait_ms( 1000 );
-//      d << "\f" << n++;
-//      d << "\t0002" << "score : " << 5 - n;
-//      oled.flush();
-//   }      
+   auto window = hwlib::terminal_from( oled, font );   //maakt terminal aan
    
-      auto log = logger( oled, d );
-     auto displa = display( log );
-     rtos::run();
-      
+   auto display = Display( oled, window );
+   display.write_int_to_channel(1);
+   display.write_int_to_channel(60);
+   display.write_int_to_channel(2);
+   display.write_int_to_channel(3);
+   display.write_int_to_channel(4);
+   rtos::run();
 }
 
